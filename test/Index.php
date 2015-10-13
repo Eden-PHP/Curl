59af89552e9dcbb0d3288036c8d7759def9be30b
Christian Blanquera <cblanquera@openovate.com>
2015-10-13 11:15:35 +0800

    Masthead and Javadoc updates

--- test/Index.php
+++ test/Index.php
@@ -2,7 +2,7 @@
 
 //-->
 /**
- * This file is part of the Eden package.
+ * This file is part of the Eden PHP Library.
  * (c) 2014-2016 Openovate Labs
  *
  * Copyright and license information can be found at LICENSE.txt
